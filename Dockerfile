92af7deacb57a694f3868e0ad769ea65c43486be
Your Name <you@example.com>
2025-11-19 14:52:37 +0000

    Refactor Docker setup and improve environment variable handling

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM node:13-alpine
-
-ENV MONGO_DB_USERNAME=admin \
-    MONGO_DB_PWD=password
-
-RUN mkdir -p /home/app
-
-COPY ./app /home/app
+FROM node:22-alpine
 
 # set default dir so that next commands executes in /home/app dir
 WORKDIR /home/app
 
+# copy package files first for better layer caching
+COPY ./app/package*.json ./
+
 # will execute npm install in /home/app because of WORKDIR
+# this layer is cached unless package.json changes
 RUN npm install
+
+# copy the rest of the application code
+COPY ./app ./
 
 # no need for /home/app/server.js because of WORKDIR
 CMD ["node", "server.js"]
--- app/server.js
+++ app/server.js
@@ -20,14 +20,8 @@ app.get('/profile-picture', function (req, res) {
   res.end(img, 'binary');
 });
 
-// use when starting application locally with node command
-let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
-
-// use when starting application as a separate docker container
-let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
-
-// use when starting application as docker container, part of docker-compose
-let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
+// default to the local Mongo instance; override via MONGO_URL when containerised
+let mongoUrl = process.env.MONGO_URL || "mongodb://admin:password@localhost:27017";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
@@ -38,7 +32,7 @@ let databaseName = "my-db";
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
@@ -60,7 +54,7 @@ app.post('/update-profile', function (req, res) {
 app.get('/get-profile', function (req, res) {
   let response = {};
   // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
@@ -81,4 +75,3 @@ app.get('/get-profile', function (req, res) {
 app.listen(3000, function () {
   console.log("app listening on port 3000!");
 });
-
